c83a0baf96c360c2d54c1e308b00ed388801dd74
Josh Hanna <josh@hanna.io>
2014-10-18 17:02:57 -0400

    changed stardog config to query over all graphs by default

--- provision/stardog/install.sh
+++ provision/stardog/install.sh
@@ -14,21 +14,22 @@ sudo rm -rf $APPDIR
 
 #Make app directory
 sudo mkdir -p $APPDIR
-#Make data directory
-sudo mkdir -p $DATADIR
 
 #Setup permissions and switch to app dir.
 sudo chown -R vagrant:vagrant $APPDIR
 
-#Copy license 
-cp /home/vagrant/provision/stardog/stardog-license-key.bin $DATADIR
-
 #copy stardog libraries
 cp -r /home/vagrant/provision/stardog/stardog_dist/* $APPDIR
+
+#Copy license 
+cp /home/vagrant/provision/stardog/stardog-license-key.bin $DATADIR
 
 cd $APPDIR
 export STARDOG_HOME=$DATADIR
 
+#have to start the server with security disabled
+#since vivo does not allow you to provide credentials
 ./bin/stardog-admin server start --disable-security
 
+#create vivo DB 
 ./bin/stardog-admin db create -n vivo
