d8a2f6be50bc978152c2722ec4fade999e4ec700
Bruno S. Barros <brunosoab@gmail.com>
2014-12-19 12:01:48 -0200

    Adds [spr41929] STR() aggregation fix (AllegroGraph):
    The issue has to do with a bad interaction between the STR function and
    the translation that the query engine makes between RDF datatypes (like
    "3"^^xsd:integer) and the datatypes in the underlying implementation
    (like 3). The STR function knows how to handle the RDF datatypes but not
    the implementation dependent ones and the aggregation function is
    passing the underlying type to STR and so much sadness ensues.

--- provision/allegrograph/install.sh
+++ provision/allegrograph/install.sh
@@ -12,14 +12,18 @@ sudo gem install json agraph --no-ri
 cat /home/vagrant/provision/allegrograph/license_ag.txt
 
 # AllegroGraph 4.14.1
-sudo wget http://franz.com/ftp/pri/acl/ag/ag4.14.1/linuxamd64.64/agraph-4.14.1-linuxamd64.64.tar.gz
-sudo tar zxf agraph-4.14.1-linuxamd64.64.tar.gz
+wget http://franz.com/ftp/pri/acl/ag/ag4.14.1/linuxamd64.64/agraph-4.14.1-linuxamd64.64.tar.gz
+tar zxf agraph-4.14.1-linuxamd64.64.tar.gz
 
 # AllegroGraph installation (non interactive mode)
-sudo agraph-4.14.1/configure-agraph --non-interactive --runas-user agraph --create-runas-user --super-user test --super-password xyzzy
+agraph-4.14.1/configure-agraph --non-interactive --runas-user agraph --create-runas-user --super-user test --super-password xyzzy
+
+# AllegroGraph permissions
+sudo chown -R agraph:agraph /home/vagrant/data
+sudo chown -R agraph:agraph /home/vagrant/log
 
 # AllegroGraph service
-sudo agraph-4.14.1/agraph-control --config agraph-4.14.1/agraph.cfg start
+agraph-4.14.1/agraph --config agraph-4.14.1/agraph.cfg --load-file /home/vagrant/provision/allegrograph/patch-spr41929.fasl
 
 # AllegroGraph client (creates a new repository)
 ruby /home/vagrant/provision/allegrograph/create_vivo_repository.rb
