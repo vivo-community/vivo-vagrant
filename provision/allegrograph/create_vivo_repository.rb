f78e0e440f920bdc6b7229fef4593948bb7e93af
Ted Lawless <lawlesst@gmail.com>
2014-12-29 14:55:46 -0500

    Remove Ruby script.

--- provision/allegrograph/create_vivo_repository.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'allegro_graph'
-
-server = AllegroGraph::Server.new :username => "test", :password => "xyzzy"
-repository = AllegroGraph::Repository.new server, "vivo"
-repository.create_if_missing!
