8a0c9f930e12f6d37b07dee3f653814839a5ba51
Darren L. Weber, Ph.D. <dweber.consulting@gmail.com>
2015-10-06 18:29:52 -0700

    Update reset_vivo_db.sh
    Remove Solr data so the VIVO restart will re-index to clean up the UI.

--- provision/reset_vivo_db.sh
+++ provision/reset_vivo_db.sh
@@ -7,8 +7,13 @@ set -e
 cd  # start at $HOME path
 sudo service tomcat7 stop
 VIVO_DATABASE=$(grep 'VIVO_DATABASE=' provision/vivo/install.sh | sed s/VIVO.*=// )
+# save the database schema (without data)
 mysqldump -uroot -pvivo --single-transaction --no-data $VIVO_DATABASE > "${VIVO_DATABASE}_schema.sql"
+# destroy the database
 mysql -uroot -pvivo -e "DROP DATABASE IF EXISTS $VIVO_DATABASE;"
+# recreate the database
 mysql -uroot -pvivo -e "CREATE DATABASE $VIVO_DATABASE DEFAULT CHARACTER SET utf8;"
 mysql -uroot -pvivo $VIVO_DATABASE < "${VIVO_DATABASE}_schema.sql"
+# remove solr data so vivo will reindex on startup
+sudo rm -rf /usr/local/vdata/solr/data
 sudo service tomcat7 start
